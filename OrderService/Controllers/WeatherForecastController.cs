5ce1a75db402c4156365ccb7f5808bb03b793afa
pjain <pjain@workforcesoftware.com>
2026-04-13 18:29:21 +0530

    application insights changes, key add and exception thrown.

--- OrderService/Controllers/WeatherForecastController.cs
+++ OrderService/Controllers/WeatherForecastController.cs
@@ -1,17 +1,11 @@
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 
 namespace OrderService.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("OrderService")]
     public class WeatherForecastController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
         private readonly ILogger<WeatherForecastController> _logger;
 
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
@@ -23,27 +17,20 @@ namespace OrderService.Controllers
         public IEnumerable<WeatherForecast> Get()
         {
             _logger.LogInformation("API is calling");
-            _logger.LogInformation("This is an Information log"); 
-            _logger.LogWarning("This is a Warning log"); 
+            _logger.LogInformation("This is an Information log");
+            _logger.LogWarning("This is a Warning log");
             _logger.LogError("This is an Error log");
 
             try
             {
-                return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-                {
-                    Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    TemperatureC = Random.Shared.Next(-20, 55),
-                    Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-                })
-                .ToArray();
-
+                throw new Exception("Test exception");
             }
             catch (Exception ex)
             {
                 _logger.LogError(ex, "Order creation failed for OrderId");
-                throw;
+                return null;
             }
-          
+
         }
     }
 }
